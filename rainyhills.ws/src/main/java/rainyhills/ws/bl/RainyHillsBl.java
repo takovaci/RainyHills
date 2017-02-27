68c74bbd243d21a26e684d27e94119bf8d65bb9c
tadej <tadej@fata-mint>
2017-02-27 19:16:03 +0100

    redone algoritem, adding tests and setting java version to webservice client

--- rainyhills.ws/src/main/java/rainyhills/ws/bl/RainyHillsBl.java
+++ rainyhills.ws/src/main/java/rainyhills/ws/bl/RainyHillsBl.java
@@ -8,45 +8,34 @@ import javax.ejb.Stateless;
 public class RainyHillsBl {
 
 	public int getRainyHillsVolume(int[] inList) {
-		int result = 0;
-		int maxHeight = 0;
-		int previousIndex = 0;
-		int previousValue = 0;
-		int partialVolume = 0;
-		if (inList == null || inList.length < 3) {
-			return result;
+		int result = 0; //result is zero until we collect some water
+		int index = 0; //temp index put 0;
+		int maxValue = inList[0]; //we store max value, at start is this first value;
+		int potentialVolume = 0; // for storing partial water 
+		for (int i = 1 ; i < inList.length ; i++){ //going from left to right
+			if(maxValue > inList[i]){ //collecting all water left from highest hill
+				potentialVolume += (maxValue-inList[i]); //we temporary store difference between current hill and max hill 
+			}else{ //we store water and index if we found higher hill
+				result +=potentialVolume;
+				potentialVolume = 0;
+				maxValue = inList[i];
+				index = i;
+			}
 		}
-		//fin first peak, after that water can start collecting
-		while(inList[previousIndex] > maxHeight) {
-	        maxHeight = inList[previousIndex];
-	        previousIndex++;
-	        if(previousIndex==inList.length)            // if every next value is higher then we don't collect any water )
-	            return result;
-	        else
-	        	previousValue = inList[previousIndex];
-	    }
+		if(potentialVolume > 0){ //we check right from highest hill only if we have some potential water
+			maxValue = inList[inList.length-1]; //now we set highest value last value in list
+			potentialVolume = 0; //reset potential water
+			for(int i = inList.length-2 ; i >=index; i--){ //we check from second to last index to highest in the table
+				if(maxValue > inList[i]){ // potential water
+					potentialVolume += (maxValue - inList[i]);
+				} else { //we store potential water if we found higher hill
+					result += potentialVolume;
+					potentialVolume = 0;
+					maxValue = inList[i]; 
+				}
+			}
+		}
 		
-		for(int i = previousIndex; i<inList.length; i++) {
-	        if(inList[i] >= maxHeight) {      // collect all partialVolume water
-	            result += partialVolume;
-	            partialVolume = 0;
-	            maxHeight = inList[i];        // set new max height
-	        }
-	        else {
-	        	partialVolume += maxHeight - inList[i];
-	            if(inList[i] > previousValue) {  // we went up... collect some water
-	                int collWater = (i-previousIndex)*(inList[i]-previousValue);
-	                result += collWater;
-	                partialVolume -= collWater;
-	            }
-	        }
-
-	        // previousHeight only changes if consecutive towers are not same height
-	        if(inList[i] != previousValue) {
-	        	previousValue = inList[i];
-	        	previousIndex = i;
-	        }
-	    }
 		return result;
 	}
 }
--- rainyhills.wsc/src/test/java/rainyhills/wsc/test/TestWsc.java
+++ rainyhills.wsc/src/test/java/rainyhills/wsc/test/TestWsc.java
@@ -24,7 +24,7 @@ public class TestWsc {
 		
 		List<Integer> inList = new ArrayList<>(Arrays.asList(3,2,4,1,2));
 		int rainyHillsVolume = wsc.getRainyHillsVolume(inList);
-		Assert.assertEquals("Not corret value", 2, rainyHillsVolume, 0);
+		Assert.assertEquals("Not correct value", 2, rainyHillsVolume, 0);
 	}
 	
 	@Test
@@ -32,7 +32,41 @@ public class TestWsc {
 		
 		List<Integer> inList = new ArrayList<>(Arrays.asList(4,1,1,0,2,3));
 		int rainyHillsVolume = wsc.getRainyHillsVolume(inList);
-		Assert.assertEquals("Not corret value", 8, rainyHillsVolume, 0);
+		Assert.assertEquals("Not correct value", 8, rainyHillsVolume, 0);
 	}
 
+	@Test
+	public void thirdTest(){
+		List<Integer> inList = new ArrayList<>(Arrays.asList(7,4,0,9));
+		int rainyHillsVolume = wsc.getRainyHillsVolume(inList);
+		Assert.assertEquals("Not correct value", 10, rainyHillsVolume, 0);
+	}
+	
+	@Test
+	public void fourthTest(){
+		List<Integer> inList = new ArrayList<>(Arrays.asList(6,9,9));
+		int rainyHillsVolume = wsc.getRainyHillsVolume(inList);
+		Assert.assertEquals("Not correct value", 0, rainyHillsVolume, 0);
+	}
+	
+	@Test
+	public void fifthTest(){
+		List<Integer> inList = new ArrayList<>(Arrays.asList(6,2,9));
+		int rainyHillsVolume = wsc.getRainyHillsVolume(inList);
+		Assert.assertEquals("Not correct value", 4, rainyHillsVolume, 0);
+	}
+	
+	@Test
+	public void sixtTest(){
+		List<Integer> inList = new ArrayList<>(Arrays.asList(1,5,3,7,2));
+		int rainyHillsVolume = wsc.getRainyHillsVolume(inList);
+		Assert.assertEquals("Not correct value", 2, rainyHillsVolume, 0);
+	}
+	
+	@Test
+	public void seventhTest(){
+		List<Integer> inList = new ArrayList<>(Arrays.asList(5,3,7,2,6,4,5,9,1,2));
+		int rainyHillsVolume = wsc.getRainyHillsVolume(inList);
+		Assert.assertEquals("Not correct value", 14, rainyHillsVolume, 0);
+	}
 }
